15b43a1e6b771bc3419bc0a621670de3def1b492
Sakil Khan <sakilk130@gmail.com>
2023-08-04 12:21:40 +0600

    feat: category page

--- actions/index.ts
+++ actions/index.ts
@@ -2,3 +2,6 @@ export { getCategories } from './get-categories';
 export { getBillboard } from './get-billboard';
 export { getProducts } from './get-products';
 export { getProduct } from './get-product';
+export { getCategory } from './get-category';
+export { getSizes } from './get-sizes';
+export { getColors } from './get-colors';
